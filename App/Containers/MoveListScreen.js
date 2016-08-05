9db40d7d7f6b37a72039f089f067742f4fbb9f72
tgaff <tgaff@alumni.nd.edu>
2016-08-04 18:15:33 -0700

    updated data with July31st update and now rendering move info to screen

--- App/Containers/MoveListScreen.js
+++ App/Containers/MoveListScreen.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react'
 import { ScrollView, Text, ListView, TouchableHighlight } from 'react-native'
 import { connect } from 'react-redux'
-
+import {CINEMATIC_MOVES, QUICK_MOVES} from '../Lib/MoveData'
 // Styles
 import styles from './Styles/MoveListStyle'
 
@@ -10,16 +10,44 @@ class MoveListScreen extends React.Component {
   constructor (props) {
     super(props)
 
+    // const quickMoves = this.props.data.QuickMoves
+    console.log('props: ', props)
+
+    const allMoves = {
+      powerMoves: CINEMATIC_MOVES,
+      quickMoves: QUICK_MOVES
+    }
+    console.log('I have quickmoves: ',this.props.data.subdata.quickMoves)
+    myQuickMoveInfo = this.props.data.subdata.quickMoves.map(function(move) {
+      return allMoves.quickMoves.find(function(item) {
+        console.log('compare item.name:', item.name, ' with move: ', move)
+        return item.name.toLowerCase() == move.name.toLowerCase()
+      })
+    })
+    //myQuickMoveInfo = [...new Set(myQuickMoveInfo)] // FIXME why aren't moves already unique?
+
+    console.log('I have powermoves: ',this.props.data.subdata.quickMoves)
+    myPowerMoveInfo = this.props.data.subdata.powerMoves.map(function(move) {
+      return allMoves.powerMoves.find(function(item) {
+        console.log('compare item.name:', item.name, ' with move: ', move)
+        return item.name.toLowerCase() == move.name.toLowerCase()
+      })
+    })
+    //myPowerMoveInfo = [...new Set(myPowerMoveInfo)] // FIXME why aren't moves already unique?
+
+
+    // console.log('quick moves', this.props.data.QuickMoves)
+    // Datasource is always in state
     const rowHasChanged = (r1, r2) => r1 !== r2
     const ds = new ListView.DataSource({rowHasChanged})
-    // const quickMoves = this.props.data.QuickMoves
-    console.log('proops: ', props)
-    const powerMoves = this.props.data.CinematicMoves
-    console.log('quick moves', this.props.data.QuickMoves)
+
     this.state = {
-      quickMoves: ds.cloneWithRows(this.props.data.QuickMoves),
-      powerMoves: ds.cloneWithRows(powerMoves)
+      myMoves: {
+        quickMoves: ds.cloneWithRows(myQuickMoveInfo),
+        powerMoves: ds.cloneWithRows(myPowerMoveInfo)
+      }
     }
+
     // interesting stuff
     // console.log('routes', props.navigator.state.routeStack)
     // console.log(this.props.navigator.navigationContext.currentRoute)
@@ -33,8 +61,9 @@ class MoveListScreen extends React.Component {
 
 
   _renderRow = (rowData) => {
+    console.log('row', rowData)
     return (  <TouchableHighlight onPress={ () => console.log('clicked')  }>
-              <Text style={styles.item}>({rowData})</Text>
+              <Text style={styles.item}>({rowData.name} - {rowData.dps})</Text>
             </TouchableHighlight>
           )
   }
@@ -50,28 +79,37 @@ class MoveListScreen extends React.Component {
         <ListView
             initialListSize={100}
             contentContainerStyle={styles.listContent}
-            dataSource={this.state.quickMoves}
+            dataSource={this.state.myMoves.quickMoves}
             renderRow={this._renderRow}
         />
         <Text style={styles.text}>Power moves</Text>
-          <ListView
-              initialListSize={100}
-              contentContainerStyle={styles.listContent}
-              dataSource={this.state.powerMoves}
-              renderRow={this._renderRow}
-          />
+        <ListView
+            initialListSize={100}
+            contentContainerStyle={styles.listContent}
+            dataSource={this.state.myMoves.powerMoves}
+            renderRow={this._renderRow}
+        />
 
       </ScrollView>
     )
   }
 }
 
+// map state passed through as part of state
 const mapStateToProps = (state) => {
   console.log('mapStateToProps')
   console.log(state)
-  return {
-    someState: 'foo'
-  }
+  // const myMoves = {
+  //   powerMoves: this.props.data.CinematicMoves,
+  //   quickMoves: this.props.data.QuickMoves
+  // }
+  return { }
+  //   allMoves: {
+  //     powerMoves: CINEMATIC_MOVES,
+  //     quickMoves: QUICK_MOVES,
+  //   },
+  //   myMoves: myMoves
+  // }
 }
 
 export default connect(mapStateToProps)(MoveListScreen)
---
+++ App/Lib/MoveData.js
@@ -0,0 +1,872 @@
+export const QUICK_MOVES = [
+  {
+     "id":1
+    ,"name":"Pound"
+    ,"type":"Normal"
+    ,"damage":7
+    ,"dps":13
+  },
+  {
+     "id":2
+    ,"name":"Metal claw"
+    ,"type":"Steel"
+    ,"damage":8
+    ,"dps":12.7
+  },
+  {
+     "id":3
+    ,"name":"Psycho cut"
+    ,"type":"Psychic"
+    ,"damage":7
+    ,"dps":12.3
+  },
+  {
+     "id":4
+    ,"name":"Bite"
+    ,"type":"Dark"
+    ,"damage":6
+    ,"dps":12
+  },
+  {
+     "id":5
+    ,"name":"Dragon breath"
+    ,"type":"Dragon"
+    ,"damage":6
+    ,"dps":12
+  },
+  {
+     "id":6
+    ,"name":"Wing attack"
+    ,"type":"Flying"
+    ,"damage":9
+    ,"dps":12
+  },
+  {
+     "id":7
+    ,"name":"Scratch"
+    ,"type":"Normal"
+    ,"damage":6
+    ,"dps":12
+  },
+  {
+     "id":8
+    ,"name":"Water gun"
+    ,"type":"Water"
+    ,"damage":6
+    ,"dps":12
+  },
+  {
+     "id":9
+    ,"name":"Fire fang"
+    ,"type":"Fire"
+    ,"damage":10
+    ,"dps":11.9
+  },
+  {
+     "id":10
+    ,"name":"Shadow claw"
+    ,"type":"Ghost"
+    ,"damage":11
+    ,"dps":11.6
+  },
+  {
+     "id":11
+    ,"name":"Feint attack"
+    ,"type":"Dark"
+    ,"damage":12
+    ,"dps":11.5
+  },
+  {
+     "id":12
+    ,"name":"Poison jab"
+    ,"type":"Poison"
+    ,"damage":12
+    ,"dps":11.4
+  },
+  {
+     "id":13
+    ,"name":"Zen headbutt"
+    ,"type":"Psychic"
+    ,"damage":12
+    ,"dps":11.4
+  },
+  {
+     "id":14
+    ,"name":"Steel wing"
+    ,"type":"Steel"
+    ,"damage":15
+    ,"dps":11.3
+  },
+  {
+     "id":15
+    ,"name":"Bug bite"
+    ,"type":"Bug"
+    ,"damage":5
+    ,"dps":11.1
+  },
+  {
+     "id":16
+    ,"name":"Frost breath"
+    ,"type":"Ice"
+    ,"damage":9
+    ,"dps":11.1
+  },
+  {
+     "id":17
+    ,"name":"Mud slap"
+    ,"type":"Ground"
+    ,"damage":15
+    ,"dps":11.1
+  },
+  {
+     "id":18
+    ,"name":"Mud shot"
+    ,"type":"Ground"
+    ,"damage":6
+    ,"dps":10.9
+  },
+  {
+     "id":19
+    ,"name":"Tackle"
+    ,"type":"Normal"
+    ,"damage":12
+    ,"dps":10.9
+  },
+  {
+     "id":20
+    ,"name":"Bubble"
+    ,"type":"Water"
+    ,"damage":25
+    ,"dps":10.9
+  },
+  {
+     "id":21
+    ,"name":"Vine whip"
+    ,"type":"Grass"
+    ,"damage":7
+    ,"dps":10.8
+  },
+  {
+     "id":22
+    ,"name":"Ice shard"
+    ,"type":"Ice"
+    ,"damage":15
+    ,"dps":10.7
+  },
+  {
+     "id":23
+    ,"name":"Cut"
+    ,"type":"Normal"
+    ,"damage":12
+    ,"dps":10.6
+  },
+  {
+     "id":24
+    ,"name":"Rock smash"
+    ,"type":"Fighting"
+    ,"damage":15
+    ,"dps":10.6
+  },
+  {
+     "id":25
+    ,"name":"Poison sting"
+    ,"type":"Poison"
+    ,"damage":6
+    ,"dps":10.4
+  },
+  {
+     "id":26
+    ,"name":"Razor leaf"
+    ,"type":"Grass"
+    ,"damage":15
+    ,"dps":10.3
+  },
+  {
+     "id":27
+    ,"name":"Sucker punch"
+    ,"type":"Dark"
+    ,"damage":7
+    ,"dps":10
+  },
+  {
+     "id":28
+    ,"name":"Spark"
+    ,"type":"Electric"
+    ,"damage":7
+    ,"dps":10
+  },
+  {
+     "id":29
+    ,"name":"Lick"
+    ,"type":"Ghost"
+    ,"damage":5
+    ,"dps":10
+  },
+  {
+     "id":30
+    ,"name":"Confusion"
+    ,"type":"Psychic"
+    ,"damage":15
+    ,"dps":9.9
+  },
+  {
+     "id":31
+    ,"name":"Ember"
+    ,"type":"Fire"
+    ,"damage":10
+    ,"dps":9.5
+  },
+  {
+     "id":32
+    ,"name":"Acid"
+    ,"type":"Poison"
+    ,"damage":10
+    ,"dps":9.5
+  },
+  {
+     "id":33
+    ,"name":"Rock throw"
+    ,"type":"Rock"
+    ,"damage":12
+    ,"dps":8.8
+  },
+  {
+     "id":34
+    ,"name":"Peck"
+    ,"type":"Flying"
+    ,"damage":10
+    ,"dps":8.7
+  },
+  {
+     "id":35
+    ,"name":"Thunder shock"
+    ,"type":"Electric"
+    ,"damage":5
+    ,"dps":8.3
+  },
+  {
+     "id":36
+    ,"name":"Low kick"
+    ,"type":"Fighting"
+    ,"damage":5
+    ,"dps":8.3
+  },
+  {
+     "id":37
+    ,"name":"Bullet punch"
+    ,"type":"Steel"
+    ,"damage":10
+    ,"dps":8.3
+  },
+  {
+     "id":38
+    ,"name":"Fury cutter"
+    ,"type":"Bug"
+    ,"damage":3
+    ,"dps":7.5
+  },
+  {
+     "id":39
+    ,"name":"Karate chop"
+    ,"type":"Fighting"
+    ,"damage":6
+    ,"dps":7.5
+  },
+  {
+     "id":40
+    ,"name":"Quick attack"
+    ,"type":"Normal"
+    ,"damage":10
+    ,"dps":7.5
+  },
+  {
+     "id":41
+    ,"name":"Splash"
+    ,"type":"Water"
+    ,"damage":0
+    ,"dps":0
+  }]
+
+
+export const CINEMATIC_MOVES = [
+  {
+     "id":1
+    ,"name":"Stone edge"
+    ,"type":"Rock"
+    ,"damage":80
+    ,"dps":32.3
+  },
+  {
+     "id":2
+    ,"name":"Cross chop"
+    ,"type":"Fighting"
+    ,"damage":55
+    ,"dps":30.9
+  },
+  {
+     "id":3
+    ,"name":"Blizzard"
+    ,"type":"Ice"
+    ,"damage":100
+    ,"dps":26.3
+  },
+  {
+     "id":4
+    ,"name":"Dragon claw"
+    ,"type":"Dragon"
+    ,"damage":35
+    ,"dps":26.3
+  },
+  {
+     "id":5
+    ,"name":"Body slam"
+    ,"type":"Normal"
+    ,"damage":40
+    ,"dps":26.3
+  },
+  {
+     "id":6
+    ,"name":"Megahorn"
+    ,"type":"Bug"
+    ,"damage":80
+    ,"dps":25.6
+  },
+  {
+     "id":7
+    ,"name":"Hurricane"
+    ,"type":"Flying"
+    ,"damage":80
+    ,"dps":25.6
+  },
+  {
+     "id":8
+    ,"name":"Solar beam"
+    ,"type":"Grass"
+    ,"damage":120
+    ,"dps":25.1
+  },
+  {
+     "id":9
+    ,"name":"Fire blast"
+    ,"type":"Fire"
+    ,"damage":100
+    ,"dps":25
+  },
+  {
+     "id":10
+    ,"name":"Power whip"
+    ,"type":"Grass"
+    ,"damage":70
+    ,"dps":25
+  },
+  {
+     "id":11
+    ,"name":"Hyper beam"
+    ,"type":"Normal"
+    ,"damage":120
+    ,"dps":24.6
+  },
+  {
+     "id":12
+    ,"name":"Earthquake"
+    ,"type":"Ground"
+    ,"damage":100
+    ,"dps":24.4
+  },
+  {
+     "id":13
+    ,"name":"Hydro pump"
+    ,"type":"Water"
+    ,"damage":90
+    ,"dps":24.3
+  },
+  {
+     "id":14
+    ,"name":"Thunder"
+    ,"type":"Electric"
+    ,"damage":100
+    ,"dps":23.8
+  },
+  {
+     "id":15
+    ,"name":"Gunk shot"
+    ,"type":"Poison"
+    ,"damage":65
+    ,"dps":22.2
+  },
+  {
+     "id":16
+    ,"name":"Heat wave"
+    ,"type":"Fire"
+    ,"damage":80
+    ,"dps":21.6
+  },
+  {
+     "id":17
+    ,"name":"Moonblast"
+    ,"type":"Fairy"
+    ,"damage":85
+    ,"dps":21.3
+  },
+  {
+     "id":18
+    ,"name":"Sludge wave"
+    ,"type":"Poison"
+    ,"damage":70
+    ,"dps":21.1
+  },
+  {
+     "id":19
+    ,"name":"Brick break"
+    ,"type":"Fighting"
+    ,"damage":30
+    ,"dps":21.1
+  },
+  {
+     "id":20
+    ,"name":"Thunderbolt"
+    ,"type":"Electric"
+    ,"damage":55
+    ,"dps":20.9
+  },
+  {
+     "id":21
+    ,"name":"Petal blizzard"
+    ,"type":"Grass"
+    ,"damage":65
+    ,"dps":20.8
+  },
+  {
+     "id":22
+    ,"name":"Psychic"
+    ,"type":"Psychic"
+    ,"damage":55
+    ,"dps":20.1
+  },
+  {
+     "id":23
+    ,"name":"Sludge bomb"
+    ,"type":"Poison"
+    ,"damage":50
+    ,"dps":19.7
+  },
+  {
+     "id":24
+    ,"name":"Aqua tail"
+    ,"type":"Water"
+    ,"damage":45
+    ,"dps":19.6
+  },
+  {
+     "id":25
+    ,"name":"Flamethrower"
+    ,"type":"Fire"
+    ,"damage":55
+    ,"dps":19.4
+  },
+  {
+     "id":26
+    ,"name":"Play rough"
+    ,"type":"Fairy"
+    ,"damage":55
+    ,"dps":19.4
+  },
+  {
+     "id":27
+    ,"name":"Cross poison"
+    ,"type":"Poison"
+    ,"damage":25
+    ,"dps":18.8
+  },
+  {
+     "id":28
+    ,"name":"Dragon pulse"
+    ,"type":"Dragon"
+    ,"damage":65
+    ,"dps":18.5
+  },
+  {
+     "id":29
+    ,"name":"Ice beam"
+    ,"type":"Ice"
+    ,"damage":65
+    ,"dps":18.3
+  },
+  {
+     "id":30
+    ,"name":"Bug buzz"
+    ,"type":"Bug"
+    ,"damage":75
+    ,"dps":18.1
+  },
+  {
+     "id":31
+    ,"name":"Leaf blade"
+    ,"type":"Grass"
+    ,"damage":45
+    ,"dps":18.1
+  },
+  {
+     "id":32
+    ,"name":"Seed bomb"
+    ,"type":"Grass"
+    ,"damage":40
+    ,"dps":17.1
+  },
+  {
+     "id":33
+    ,"name":"Thunder punch"
+    ,"type":"Electric"
+    ,"damage":40
+    ,"dps":17.1
+  },
+  {
+     "id":34
+    ,"name":"X scissor"
+    ,"type":"Bug"
+    ,"damage":35
+    ,"dps":17.1
+  },
+  {
+     "id":35
+    ,"name":"Hyper fang"
+    ,"type":"Normal"
+    ,"damage":35
+    ,"dps":17.1
+  },
+  {
+     "id":36
+    ,"name":"Drill run"
+    ,"type":"Ground"
+    ,"damage":50
+    ,"dps":16.5
+  },
+  {
+     "id":37
+    ,"name":"Flash cannon"
+    ,"type":"Steel"
+    ,"damage":60
+    ,"dps":15.8
+  },
+  {
+     "id":38
+    ,"name":"Iron head"
+    ,"type":"Steel"
+    ,"damage":30
+    ,"dps":15.4
+  },
+  {
+     "id":39
+    ,"name":"Drill peck"
+    ,"type":"Flying"
+    ,"damage":40
+    ,"dps":15.2
+  },
+  {
+     "id":40
+    ,"name":"Psyshock"
+    ,"type":"Psychic"
+    ,"damage":40
+    ,"dps":15.2
+  },
+  {
+     "id":41
+    ,"name":"Shadow ball"
+    ,"type":"Ghost"
+    ,"damage":45
+    ,"dps":15
+  },
+  {
+     "id":42
+    ,"name":"Submission"
+    ,"type":"Fighting"
+    ,"damage":30
+    ,"dps":14.6
+  },
+  {
+     "id":43
+    ,"name":"Fire punch"
+    ,"type":"Fire"
+    ,"damage":40
+    ,"dps":14.6
+  },
+  {
+     "id":44
+    ,"name":"Stomp"
+    ,"type":"Normal"
+    ,"damage":30
+    ,"dps":14.6
+  },
+  {
+     "id":45
+    ,"name":"Discharge"
+    ,"type":"Electric"
+    ,"damage":35
+    ,"dps":14.4
+  },
+  {
+     "id":46
+    ,"name":"Power gem"
+    ,"type":"Rock"
+    ,"damage":40
+    ,"dps":14.1
+  },
+  {
+     "id":47
+    ,"name":"Low sweep"
+    ,"type":"Fighting"
+    ,"damage":30
+    ,"dps":13.7
+  },
+  {
+     "id":48
+    ,"name":"Dazzling gleam"
+    ,"type":"Fairy"
+    ,"damage":55
+    ,"dps":13.4
+  },
+  {
+     "id":49
+    ,"name":"Dark pulse"
+    ,"type":"Dark"
+    ,"damage":45
+    ,"dps":13.2
+  },
+  {
+     "id":50
+    ,"name":"Ice punch"
+    ,"type":"Ice"
+    ,"damage":45
+    ,"dps":13.2
+  },
+  {
+     "id":51
+    ,"name":"Rock slide"
+    ,"type":"Rock"
+    ,"damage":40
+    ,"dps":12.8
+  },
+  {
+     "id":52
+    ,"name":"Bone club"
+    ,"type":"Ground"
+    ,"damage":20
+    ,"dps":12.8
+  },
+  {
+     "id":53
+    ,"name":"Night slash"
+    ,"type":"Dark"
+    ,"damage":30
+    ,"dps":12.5
+  },
+  {
+     "id":54
+    ,"name":"Dig"
+    ,"type":"Ground"
+    ,"damage":70
+    ,"dps":12.4
+  },
+  {
+     "id":55
+    ,"name":"Flame burst"
+    ,"type":"Fire"
+    ,"damage":25
+    ,"dps":12.2
+  },
+  {
+     "id":56
+    ,"name":"Mud bomb"
+    ,"type":"Ground"
+    ,"damage":30
+    ,"dps":11.8
+  },
+  {
+     "id":57
+    ,"name":"Signal beam"
+    ,"type":"Bug"
+    ,"damage":35
+    ,"dps":11.6
+  },
+  {
+     "id":58
+    ,"name":"Aqua jet"
+    ,"type":"Water"
+    ,"damage":25
+    ,"dps":10.9
+  },
+  {
+     "id":59
+    ,"name":"Water pulse"
+    ,"type":"Water"
+    ,"damage":35
+    ,"dps":10.9
+  },
+  {
+     "id":60
+    ,"name":"Psybeam"
+    ,"type":"Psychic"
+    ,"damage":40
+    ,"dps":10.8
+  },
+  {
+     "id":61
+    ,"name":"Poison fang"
+    ,"type":"Poison"
+    ,"damage":25
+    ,"dps":10.7
+  },
+  {
+     "id":62
+    ,"name":"Aerial ace"
+    ,"type":"Flying"
+    ,"damage":30
+    ,"dps":10.6
+  },
+  {
+     "id":63
+    ,"name":"Bulldoze"
+    ,"type":"Ground"
+    ,"damage":35
+    ,"dps":10.6
+  },
+  {
+     "id":64
+    ,"name":"Ancient power"
+    ,"type":"Rock"
+    ,"damage":35
+    ,"dps":10
+  },
+  {
+     "id":65
+    ,"name":"Sludge"
+    ,"type":"Poison"
+    ,"damage":25
+    ,"dps":9.9
+  },
+  {
+     "id":66
+    ,"name":"Ominous wind"
+    ,"type":"Ghost"
+    ,"damage":30
+    ,"dps":9.9
+  },
+  {
+     "id":67
+    ,"name":"Twister"
+    ,"type":"Dragon"
+    ,"damage":25
+    ,"dps":9.5
+  },
+  {
+     "id":68
+    ,"name":"Horn attack"
+    ,"type":"Normal"
+    ,"damage":20
+    ,"dps":9.3
+  },
+  {
+     "id":69
+    ,"name":"Magnet bomb"
+    ,"type":"Steel"
+    ,"damage":25
+    ,"dps":9.2
+  },
+  {
+     "id":70
+    ,"name":"Draining kiss"
+    ,"type":"Fairy"
+    ,"damage":25
+    ,"dps":9.2
+  },
+  {
+     "id":71
+    ,"name":"Scald"
+    ,"type":"Water"
+    ,"damage":35
+    ,"dps":9
+  },
+  {
+     "id":72
+    ,"name":"Flame wheel"
+    ,"type":"Fire"
+    ,"damage":40
+    ,"dps":8.9
+  },
+  {
+     "id":73
+    ,"name":"Bubble beam"
+    ,"type":"Water"
+    ,"damage":25
+    ,"dps":8.8
+  },
+  {
+     "id":74
+    ,"name":"Struggle"
+    ,"type":"Normal"
+    ,"damage":15
+    ,"dps":8.8
+  },
+  {
+     "id":75
+    ,"name":"Brine"
+    ,"type":"Water"
+    ,"damage":20
+    ,"dps":8.5
+  },
+  {
+     "id":76
+    ,"name":"Air cutter"
+    ,"type":"Flying"
+    ,"damage":25
+    ,"dps":8.5
+  },
+  {
+     "id":77
+    ,"name":"Swift"
+    ,"type":"Normal"
+    ,"damage":25
+    ,"dps":8.5
+  },
+  {
+     "id":78
+    ,"name":"Rock tomb"
+    ,"type":"Rock"
+    ,"damage":25
+    ,"dps":8.3
+  },
+  {
+     "id":79
+    ,"name":"Flame charge"
+    ,"type":"Fire"
+    ,"damage":25
+    ,"dps":8.3
+  },
+  {
+     "id":80
+    ,"name":"Wrap"
+    ,"type":"Normal"
+    ,"damage":30
+    ,"dps":7.7
+  },
+  {
+     "id":81
+    ,"name":"Vice grip"
+    ,"type":"Normal"
+    ,"damage":15
+    ,"dps":7.3
+  },
+  {
+     "id":82
+    ,"name":"Icy wind"
+    ,"type":"Ice"
+    ,"damage":25
+    ,"dps":6.7
+  },
+  {
+     "id":83
+    ,"name":"Disarming voice"
+    ,"type":"Fairy"
+    ,"damage":20
+    ,"dps":5.3
+  }]
