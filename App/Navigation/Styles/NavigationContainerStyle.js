96968777ac8b844d2a199e82aebb1ac00eb0afdf
tgaff <tgaff@alumni.nd.edu>
2016-08-12 16:51:58 -0700

    help button now accessible from moves screen

--- App/Navigation/Styles/NavigationContainerStyle.js
+++ App/Navigation/Styles/NavigationContainerStyle.js
@@ -14,6 +14,13 @@ export default {
     tintColor: Colors.charcoal
   },
   rightButton: {
-    color: Colors.charcoal
+    top: 0
+  },
+  rightButtonTextStyle: {
+    color: Colors.charcoal,
+    fontSize: 22,
+    padding: 8, // weird alignment issues, had to add a top:0 on the container (rightButton above)
+    marginTop: 4,
+    textAlignVertical: 'top'
   }
 }
